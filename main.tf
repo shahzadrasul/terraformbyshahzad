64cf78989036fefd800ab7d8f4d625e095969781
Shahzad Rasul <shahzad.rasul@atomlearning.co.uk>
2022-07-01 08:42:20 +0100

    Added lambda

--- main.tf
+++ main.tf
@@ -25,20 +25,3 @@ resource "aws_s3_bucket" "bucket_b" {
   tags = var.tags
 }
 
-# Upload object to S3 bucket_a
-resource "aws_s3_bucket_object" "image1" {
-  for_each = fileset ("myfiles/","*")
-  bucket = aws_s3_bucket.bucket_a
-  key = each.value
-  source = "myfiles/${each.value}"
-  etag = filemd5 ("myfiles/${each.value}")
-  
-}
-
-# # Copy from bucket_a to bucket_b
-# resource "aws_s3_bucket "image1" {
-#   provisioner "local-exec" {
-#     command = "aws s3 cp ${var.bucket_prefix}-bucket_a ${var.bucket_prefix}-bucket_a --recursive"
-#   }
-# }
-# 
