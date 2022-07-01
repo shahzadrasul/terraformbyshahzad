559e37c0e57b89c123e3fa19703239521f32bd23
Shahzad Rasul <shahzad.rasul@atomlearning.co.uk>
2022-07-01 08:44:35 +0100

    new

---
+++ exifremove.py
@@ -0,0 +1,5 @@
+     
+#!/usr/bin/env python3
+from PIL import Image
+image = Image.open('/Users/shahzadrasul/projects/terraformbyshahzad/houseimage.jpeg')
+image.save('/Users/shahzadrasul/projects/terraformbyshahzad/houseimage.jpeg')
---
+++ lambda.py
@@ -0,0 +1,56 @@
+from PIL import Image
+import io
+import boto3, os
+
+# Create a new PIL image from the image_data:
+image = Image.open(image_data)
+
+# We need to use the orientation EXIF info to rotate the image:
+image_exif = image.getexif()
+if image_exif:
+  exif = dict(image_exif.items())
+  if exif.get(orientation) == 3:
+    image = image.rotate(180, expand=True)
+  elif exif.get(orientation) == 6:
+    image = image.rotate(270, expand=True)
+  elif exif.get(orientation) == 8:
+    image = image.rotate(90, expand=True)
+
+# Retrieve the image contents as a list representing a sequence of pixel values:
+image_contents = list(image.getdata())
+
+# Create a new image based on the original, but without the full EXIF data:
+new_image = Image.new(image.mode, image.size)
+new_image.putdata(image_contents)
+
+# Finally, create a new buffer object and put the new image file data into it:
+new_image_data = io.BytesIO()
+new_image.save(new_image_data, 'PNG') # Or the file format needed
+new_image_data.seek(0)
+
+def handler(event):
+
+  s3 = boto3.client('s3')
+
+
+  file_path = event['Records'][0]['s3']['object']['key']
+
+
+  file_name, file_extension = os.path.splitext(file_path)
+
+
+  if file_extension.lower() not in ['.jpg', '.png', '.jpeg']:
+    return
+
+  tagging = s3.get_object_tagging(Bucket='bucket_a', Key=file_path)
+  object_tags = tagging['TagSet']
+
+  for tag in object_tags:
+    if tag['Key'] == 'ExifStripped' and tag['Value'] == 'True':
+      return
+
+
+  image_data = s3.get_object(Bucket='bucket_a', Key=file_path)
+
+
+  s3.put_object(Bucket='mybucket', Key=file_path, Body=new_image_data, Tagging='ExifStripped=True')
